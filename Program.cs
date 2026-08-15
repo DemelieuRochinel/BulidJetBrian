6ddf0839220bda39128472658f7027af790ef666
LekeugodemelieuRochinel <Lekeugodemelieu@gmail.com>
2026-08-15 16:57:01 +0100

    Test the IDE

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace JetCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        var person = new Person("Rochinel", (22));
+        
+        Console.WriteLine($"my name is {person.Name} and i'm {person.Age} years old");
+    }
+}
